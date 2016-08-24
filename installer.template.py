bd432addbcb73d78c71989f8dd3b022db0b098cd
Will McGugan <willmcgugan@gmail.com>
2016-08-24 17:30:35 +0100

    installer functionality

--- installer.template.py
+++ installer.template.py
@@ -9,7 +9,7 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import argparse
-from base64 import base85decode
+import base64
 import datetime
 import json
 import os
@@ -27,8 +27,8 @@ import traceback
 # (yes, I'm shouting)
 SETTINGS = """
 {
-    "dataplicity_version": "0.4.0",
-    "base_dir": "./dataplicity/agent/"
+    "agent_version": "0.4.0",
+    "base_dir": "./__agent__/"
 }
 """
 # {% endif %}
@@ -42,9 +42,9 @@ def log(text, *args, **kwargs):
     # This will eventually go to a file, and sent back to us
     log_text = text.format(*args, **kwargs)
     time_str = datetime.datetime.utcnow().ctime()
-    lines = log.text.splitlines()
+    lines = (log_text + '\n').splitlines()
     for line in lines:
-        log_text = "{}:{}".format(time_str, line)
+        log_text = "[{}] {}".format(time_str, line)
         print(log_text)
 
 
@@ -55,16 +55,27 @@ def user(text, *args, **kwargs):
     log('USER: ' + log_text)
 
 
-def log_traceback(msg):
+def log_traceback(msg, *args, **kwargs):
     """Log a traceback."""
     tb = traceback.format_exc()
-    log('[traceback] {}', msg)
+    log('[traceback]' + msg, *args, **kwargs)
     log(tb)
+
+
+def make_dir(path):
+    """Make a directory if it does not exist."""
+    try:
+        os.makedirs(path)
+    except OSError:
+        log('[make_dir] {} (exists)', path)
+    else:
+        log('[make_dir] {} (created)', path)
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Dataplicity Agent Installer')
     args = parser.parse_args()
+    log("args={!r}", args)
     return args
 
 
@@ -74,17 +85,44 @@ def main():
         run(args)
         log('install completed')
     except Exception as e:
-        log('install failed ({})', e)
-
+        log_traceback('install failed ({})', e)
     finally:
         pass
 
 
 def run(args):
+    log('-' * 70)
     log('install started')
+    log('')
     user('Welcome to the Dataplicity Agent Installer')
-    uname = " ".join(platform.uname())
-    log(uname)
+
+    distro = " ".join(platform.linux_distribution())
+    log('[LINUX]')
+    log('distro={}', distro)
+    log('')
+
+    system, node, release, version, machine, processor = platform.uname()
+
+    log('[UNAME]')
+    log('system={!r}', system)
+    log('node={!r}', node)
+    log('release={!r}', release)
+    log('version={!r}', version)
+    log('machine={!r}', machine)
+    log('processor={!r}', processor)
+    log('')
+
+    log('[SETTINGS]')
+    for k, v in settings.items():
+        log('{}={!r}', k, v)
+    log('')
+
+    agent_dir = get_agent_dir()
+    make_dir(agent_dir)
+    agent_path = write_agent(agent_dir, 'dataplicity')
+    executable_path = get_executable_path()
+    link(agent_path, executable_path)
+    make_executable(agent_path)
 
 
 def show_step(n, msg):
@@ -97,34 +135,68 @@ def show_step(n, msg):
     log(step_msg)
 
 
-def get_agent_dir(version):
-    agent_sub_dir = "{}/".format(version)
-    agent_dir = os.path.join(settings.base_dir, agent_sub_dir)
+def get_agent_dir():
+    """Get the absolute directory where the agent will be stored."""
+    agent_dir = os.path.abspath(
+        os.path.join(
+            settings['base_dir'],
+            settings['agent_version']
+        )
+    )
     return agent_dir
 
 
-def make_agent_dir(version):
-    agent_dir = get_agent_dir(version)
-    try:
-        os.makedirs(agent_dir)
-    except OSError:
-        # Already exists
-        pass
+def get_executable_path():
+    """Get the absolute path of the dataplicity executable."""
+    executable_path = os.path.abspath(
+        os.path.join(
+            settings['base_dir'],
+            'dataplicity'
+        )
+    )
+    return executable_path
 
 
-def write_agent(agent_dir, agent_filename, agent_bytes):
-    """Atomically unpack agent."""
+def write_agent(agent_dir, agent_filename):
+    """Atomically unpack agent, return agent path."""
+    agent_bytes = base64.b64decode(AGENT)
+    log('decoded agent {} bytes', len(agent_bytes))
+
     agent_filename_temp = '~' + agent_filename
-    with open(agent_filename_temp, 'wb') as write_file:
+    path = os.path.join(agent_dir, agent_filename)
+    path_temp = os.path.join(agent_dir, agent_filename_temp)
+    log('writing {}', path_temp)
+
+    with open(path_temp, 'wb') as write_file:
         write_file.write(agent_bytes)
         os.fsync(write_file.fileno())
-    os.rename(agent_filename_temp, agent_filename)
+
+    log('renamed {}', path)
+    os.rename(path_temp, path)
+
+    return path
 
 
-# This is a base85encoded version of the agent
-AGENT = b"""
-{{INSTALLER_DATA|safe}}
-"""
+def link(agent_path, executable_path):
+    """Link current version of agent."""
+    executable_path_temp = executable_path + '~'
+    os.symlink(agent_path, executable_path_temp)
+    os.rename(executable_path_temp, executable_path)
+    log("[link] {}", executable_path)
+
+
+def make_executable(path):
+    """Make a path executable."""
+    os.chmod(path, 0o777)
+    log('[make_executable] {}', path)
+
+
+
+# This is a b64encoded version of the agent
+# {% if INSTALLER_DATA %}{% INSTALLER_DATA %}{% else %}
+with open('bin/dataplicity', 'rb') as f:
+    AGENT = base64.b64encode(f.read())
+# {% endif %}
 
 if __name__ == "__main__":
     main()
