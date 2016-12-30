43aaebcdc3e6bb4be6756c6a9cb1170be4d255f8
Mateusz Mikołajczyk <mmikolajczyk@dataplicity.com>
2016-12-30 12:15:11 +0100

    add missing docstring

--- tests/dataplicity/test_device_meta.py
+++ tests/dataplicity/test_device_meta.py
@@ -21,6 +21,10 @@ def test_get_uname():
 
 @patch('dataplicity.rpi.get_machine_type', lambda: '')
 def test_get_meta():
+    """ test for get_meta function.
+        as this function calls get_machine_type underneath (which we test
+        separately), we temporarily monkey-patch the output to empty string
+    """
     # initially, _META_CACHE should be empty
     assert device_meta._META_CACHE is None
 
