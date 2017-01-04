58b7ffd338725740f22ba8496f12d758f2fa4bc3
Mateusz Mikołajczyk <mmikolajczyk@dataplicity.com>
2017-01-05 00:29:17 +0100

    updated test code for wsclient. we are now using ws4py, and a local thread to start/stop the test client

--- tests/dataplicity/m2m/test_wsclient.py
+++ tests/dataplicity/m2m/test_wsclient.py
@@ -1,21 +1,38 @@
+import threading
+import time
+
 from dataplicity.m2m.wsclient import WSClient
-from websocket_server import wsserver_factory, WebSocket
+from websocket_server import wsserver_factory
+from ws4py.websocket import EchoWebSocket
+
+# this factory yields a fixture which we can use inside of testing functions.
+# it runs a websocket server on a separate thread, and will shutdown once the
+# test function returns.
+wsserver = wsserver_factory(EchoWebSocket)
 
 
-class SimpleEcho(WebSocket):
-    def handleMessage(self):
-        # echo message back to client
-        self.sendMessage(self.data)
+class WSClientThread(threading.Thread):
+    """since the WSClient enters a forloop, there has to be a way to control
+       its demise. A simple way of achieving it would be to use thread.
+       we can send the commands (using client._server) and when we no longer
+       need the client (i.e. at the end of the test method) we can do
+       client.close()
+    """
+    def __init__(self, uri):
+        self._server = WSClient(uri)
+        return super(WSClientThread, self).__init__(
+            name=self.__class__,
+            target=self._server.start
+        )
 
-    def handleConnected(self):
-        print(self.address, 'connected')
-
-    def handleClose(self):
-        print(self.address, 'closed')
-
-
-wsserver = wsserver_factory(SimpleEcho)
+    def close(self):
+        self._server.close()
 
 
 def test_wsclient(wsserver):
-    print('===', wsserver.uri)
+    """ this is a very basic proof that we can start and stop the builtin
+        WSClient *and* wsserver
+    """
+    client = WSClientThread(wsserver.uri)
+    time.sleep(2)
+    client.close()
