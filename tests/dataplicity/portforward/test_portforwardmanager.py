d3c7a922c27d87c3c538f08a7346d40f915dcbc3
Mateusz Mikołajczyk <mmikolajczyk@dataplicity.com>
2017-01-12 15:36:31 +0100

    unit test for PortForwardManager::open_service

--- tests/dataplicity/portforward/test_portforwardmanager.py
+++ tests/dataplicity/portforward/test_portforwardmanager.py
@@ -1,15 +1,18 @@
 from dataplicity.portforward import PortForwardManager, Service
 import pytest
-from weakref import ref
 
 
 class Client(object):
+    """ fake client class, used to test dereferencing """
     def __init__(self, m2m=None):
         self.m2m = m2m
 
 
 @pytest.fixture()
 def client():
+    """ Client fixture - please note that this will not be compatible with
+        weakref
+    """
     return Client('test-m2m')
 
 
@@ -20,7 +23,7 @@ def test_manager_m2m_returns_none_without_client():
     assert manager.client is not None
     assert manager.m2m == client.m2m
     # ok, now let's delete the client, thus forcing the weakref to not be
-    # unfulfilled.
+    # fulfilled.
     del client
     assert manager.m2m is None
 
@@ -32,6 +35,7 @@ def test_manager_m2m_returns_none_without_client():
     ("alt", 8080)
 ])
 def test_default_services_are_added_during_init(client, service, port):
+    """ unit test for PortForwardManager::init() """
     manager = PortForwardManager.init(client)
     assert isinstance(manager, PortForwardManager)
     service_object = manager.get_service(service)
@@ -40,3 +44,14 @@ def test_default_services_are_added_during_init(client, service, port):
     service_by_port = manager.get_service_on_port(port)
     assert isinstance(service_by_port, Service)
     assert service_by_port == service_object
+    # test for some non-existing services:
+    assert manager.get_service_on_port(999) is None
+    assert manager.get_service('non-existing') is None
+
+
+def test_that_using_empty_service_and_port_raises(client):
+    """ unit test for PortForwardManager::open_service """
+    manager = PortForwardManager.init(client)
+    with pytest.raises(ValueError):
+        route = ('localhost', 22, 'example.com', None)
+        manager.open_service(None, route)
