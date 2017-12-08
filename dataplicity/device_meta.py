c9329e018d679c364b3622749c75cdf78429623e
Stuart Marsh <stuartk75@gmail.com>
2017-12-08 11:22:02 +0000

    codacy

--- dataplicity/device_meta.py
+++ dataplicity/device_meta.py
@@ -44,11 +44,9 @@ def get_ip_address_list():
     try:
         interfaces = get_all_interfaces()
     except:
-        """
-        Sorry for the pokemon exception, but I don't know how
-        reliable the call is, and if it fails what it will fail with.
-        It needs some exception handling or the whole get_meta call
-        will fail
-        """
+        # Sorry for the pokemon exception, but I don't know how
+        # reliable the call is, and if it fails what it will fail with.
+        # It needs some exception handling or the whole get_meta call
+        # will fail
         return []
     return [i[1] for i in interfaces]
