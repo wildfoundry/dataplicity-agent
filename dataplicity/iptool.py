beae241e1f7d533874ef620a068a19bd1c0bb12f
Will McGugan <willmcgugan@gmail.com>
2017-12-08 15:57:16 +0000

    tidier get_all_interfaces

--- dataplicity/iptool.py
+++ dataplicity/iptool.py
@@ -4,28 +4,55 @@ import fcntl
 import struct
 import array
 
+
+# Maximum number of interfaces to return
+MAX_INTERFACES = 100
+
+
 def get_all_interfaces():
-    '''
-    returns a list of tuples, giving the network name and ip address
+    """
+    Returns a list of tuples, giving the network name and ipv4 address
     [('lo', '127.0.0.1'), ('eth0', '10.0.2.15'), ('eth1', '192.168.33.10')]
-    '''
-    is_64bits = sys.maxsize > 2**32
-    struct_size = 40 if is_64bits else 32
+
+    Uses https://linux.die.net/man/7/netdevice system call
+
+    May raise an IOError if platform is unsupported
+    """
+    _is_64bits = sys.maxsize > 2**32
+    struct_size = 40 if _is_64bits else 32
+
+    if_buffer = array.array('B', '\0' * MAX_INTERFACES * struct_size)
+    if_pointer, buffer_size = if_buffer.buffer_info()
+
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    max_possible = 8 # initial value
-    while True:
-        _bytes = max_possible * struct_size
-        names = array.array('B', '\0' * _bytes)
-        outbytes = struct.unpack('iL', fcntl.ioctl(
-            s.fileno(),
-            0x8912,  # SIOCGIFCONF
-            struct.pack('iL', _bytes, names.buffer_info()[0])
-        ))[0]
-        if outbytes == _bytes:
-            max_possible *= 2
-        else:
-            break
-    namestr = names.tostring()
-    return [(namestr[i:i+16].split('\0', 1)[0],
-             socket.inet_ntoa(namestr[i+20:i+24]))
-            for i in range(0, outbytes, struct_size)]
+    try:
+        fh = s.fileno()
+        ifaces_bytes = fcntl.ioctl(
+            fh,
+            0x8912,
+            struct.pack('iL', buffer_size, if_pointer)
+        )
+        if_buffer_size, _pointer = struct.unpack('iL', ifaces_bytes)
+
+    finally:
+        s.close()
+
+    ifaces = if_buffer.tostring()
+    interfaces = []
+    for offset in range(0, if_buffer_size, struct_size):
+        _iface = ifaces[offset:offset + struct_size]
+        name, _, _ = _iface.partition(b'\0')
+        ip = socket.inet_ntoa(_iface[20:24])  # struct sockaddr ifr_hwaddr;
+        interfaces.append((
+            name.decode('ascii', 'replace'),
+            ip.decode()
+        ))
+
+    return interfaces
+
+
+if __name__ == "__main__":
+    # python -m dataplicity.iptools
+    for name, ip in get_all_interfaces():
+        print("{}\t{}".format(name, ip))
+
