49bcc165fe39d883e606aa5c30e59a413a3adf34
Will McGugan <willmcgugan@gmail.com>
2020-02-11 17:37:13 +0000

    refine build script

--- build.sh
+++ build.sh
@@ -9,10 +9,11 @@ fi
 
 mkdir -p bin
 rm -f bin/dataplicity
-virtualenv -qq .build 
+rm -rf .build
+virtualenv -qq .build -p python3
 source .build/bin/activate
 pip -q install pex==2.1.1 subprocess32
 echo building ./bin/dataplicity
 pex dataplicity==$(python dataplicity/_version.py) --pre --python-shebang="#!/usr/bin/env python" --python=$(which python2) --python=$(which python3) -r requirements.txt -o bin/dataplicity -m dataplicity.app:main
 deactivate
-echo built dataplicity agent v`./bin/dataplicity version`
+echo built dataplicity agent v$(./bin/dataplicity version)
