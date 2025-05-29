7c355bec5e02456c10b9f01881bf8ec809e1c598
Wiktor Baura <wbaura@dataplicity.com>
2025-05-29 22:51:11 +0200

    python_requires update

--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     classifiers=classifiers,
     entry_points={"console_scripts": ["dataplicity = dataplicity.app:main"]},
     install_requires=install_requires,
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, !=3.6.*',
+    python_requires='>=2.7',
     zip_safe=True,
 )
